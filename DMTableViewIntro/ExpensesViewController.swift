b7cbfc24dc11bbc5b315ce29548e2b722557feb8
Ilyass Hasnaoui <ilyass.has@live.fr>
2023-12-07 17:24:21 -0500

    Final unit 3 commit.

--- DMTableViewIntro/ExpensesViewController.swift
+++ DMTableViewIntro/ExpensesViewController.swift
@@ -8,11 +8,16 @@
 import UIKit
 
 class ExpensesViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
+    
     
     let expenses = ["Rent", "Groceries", "Utilities", "Transportation", "Entertainment", "Clothing", "Eating Out", "Travel", "Insurance", "Phone Bill"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
     }
     
@@ -20,3 +25,22 @@ class ExpensesViewController: UIViewController {
         
     }
 }
+
+extension ExpensesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped me!")
+    }
+    
+}
+extension ExpensesViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return expenses.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = expenses[indexPath.row]
+        return cell
+    }
+}
+
